65578a7321fb1da5e8bb988903fbea4de6ce1761
Jeff McCarrell <jeff@mccarrell.org>
2024-03-02 18:21:43 -0800

    completed CLI Arguments section.

--- typer-tutorial/typer_tutorial/annotated.py
+++ typer-tutorial/typer_tutorial/annotated.py
@@ -1,7 +1,14 @@
+import random
 import typer
 from typing_extensions import Annotated
 
-def main(name: Annotated[str, typer.Argument()]):
+def get_name() -> str:
+    return random.choice(["Kenny", "Eli", "Jet"])
+
+def main(name: Annotated[str, typer.Argument(help="name of the user to greet", default_factory=get_name)]):
+    """
+    Say hi to NAME very gently.
+    """
     print(f"hiya {name}")
 
 if __name__ == "__main__":
---
+++ typer-tutorial/typer_tutorial/env_var_cli_arg.py
@@ -0,0 +1,13 @@
+import typer
+
+from typing_extensions import Annotated
+
+def main(name: Annotated[str, typer.Argument(envvar="SUPER_NAME")] = "Blech"):
+    print(f"hiya Mr. {name}")
+
+if __name__ == "__main__":
+    typer.run(main)
+
+""" (typer-tutorial-py3.9) ❯ SUPER_NAME=Glurg !py
+SUPER_NAME=Glurg python env_var_cli_arg.py
+hiya Mr. Glurg """
